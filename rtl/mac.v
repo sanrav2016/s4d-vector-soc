7df18b5572a8d798cf3f8e3963ede21ac036d175
sanrav2016 <sanrav2016@gmail.com>
2026-07-20 17:36:24 -0700

    added iterative fsm to mac, updated tb with q4.12 values

--- rtl/mac.v
+++ rtl/mac.v
@@ -1,28 +1,37 @@
+// Multiply-accumulate unit (MAC)
+// Multiplies matrix diagonal * vector element-wise and stores in result
+// in 8 clock cycles (size of matrix/vector dimensions) 
+// 
+// Sanjay Ravishankar
+
 module mac (
     input wire clk,
-    input wire [15:0] matrix [7:0],
-    input wire [15:0] vector [7:0],
-    output reg data_ready = 0,
-    output reg [15:0] result = 16'd0
+    input wire resetn,
+    input wire signed [15:0] matrix [7:0],
+    input wire signed [15:0] vector [7:0],
+    output reg data_ready,
+    output reg signed [15:0] result
 );
 
-    localparam IDLE  = 2'b00;
-    localparam COMPUTE  = 2'b01;
-    localparam DONE  = 2'b10;
-
-    reg [1:0] state = IDLE;
-
     integer i;
+    reg [3:0] idx;
+    reg signed [31:0] accumulate;
 
+    // MAC runs in 8 cycles
     always @(posedge clk) begin
-        if (!data_ready) begin
-            integer sum;
-            sum = result;
-            for (i = 0; i < 8; i = i + 1) begin
-                sum = sum + ((matrix[i] * vector[i]) >>> 12);
+        if (!resetn) begin
+            data_ready <= 1'b0;
+            idx <= 3'b0;
+            result <= 16'd0;
+            accumulate <= 32'b0;
+        end else if (!data_ready) begin
+            accumulate = accumulate + (matrix[idx] * vector[idx]);
+            if (idx == 3'd7) begin
+                result <= accumulate >>> 12;
+                data_ready <= 1'b1;
+            end else begin
+                idx <= idx + 3'b1;
             end
-            result <= sum;
-            data_ready <= 1'b1;
         end
     end
 
--- tb/accelerator_tb.v
+++ tb/accelerator_tb.v
@@ -2,34 +2,42 @@
 
 module accelerator_tb;
     reg clk = 0;
+    reg resetn = 0;
     always #5 clk = ~clk;
 
-    wire [15:0] matrix [7:0];
-    wire [15:0] vector [7:0];
-    wire [15:0] result;
+    initial begin
+        resetn = 0;
+        #20 resetn = 1; 
+    end
+
+    wire signed [15:0] matrix [7:0];
+    wire signed [15:0] vector [7:0];
+    wire signed [15:0] result;
     wire data_ready;
 
-    assign matrix[0] = 16'd1;
-    assign matrix[1] = 16'd2;
-    assign matrix[2] = 16'd3;
-    assign matrix[3] = 16'd4;
-    assign matrix[4] = 16'd5;
-    assign matrix[5] = 16'd6;
-    assign matrix[6] = 16'd7;
-    assign matrix[7] = 16'd8;
+    // Multiply by 4096 to convert to Q4.12 quantization
+    assign matrix[0] = 16'sd2130;  // 0.52 * 4096
+    assign matrix[1] = -16'sd1680; // -0.41 * 4096
+    assign matrix[2] = 16'sd1843;  // 0.45 * 4096
+    assign matrix[3] = -16'sd1106; // -0.27 * 4096
+    assign matrix[4] = 16'sd4014;  // 0.98 * 4096
+    assign matrix[5] = -16'sd1393; // -0.34 * 4096
+    assign matrix[6] = 16'sd1720;  // 0.42 * 4096
+    assign matrix[7] = 16'sd3482;  // 0.85 * 4096
 
-    assign vector[0] = 16'd1;
-    assign vector[1] = 16'd2;
-    assign vector[2] = 16'd3;
-    assign vector[3] = 16'd4;
-    assign vector[4] = 16'd5;
-    assign vector[5] = 16'd6;
-    assign vector[6] = 16'd7;
-    assign vector[7] = 16'd8;
-
+    assign vector[0] = 16'sd2130;  // 0.52 * 4096
+    assign vector[1] = -16'sd1680; // -0.41 * 4096
+    assign vector[2] = 16'sd1843;  // 0.45 * 4096
+    assign vector[3] = -16'sd1106; // -0.27 * 4096
+    assign vector[4] = 16'sd4014;  // 0.98 * 4096
+    assign vector[5] = -16'sd1393; // -0.34 * 4096
+    assign vector[6] = 16'sd1720;  // 0.42 * 4096
+    assign vector[7] = 16'sd3482;  // 0.85 * 4096
+    
     // Instantiate MAC
     mac mac_inst (
         .clk(clk),
+        .resetn(resetn),
         .matrix(matrix),
         .vector(vector),
         .result(result),
@@ -38,13 +46,15 @@ module accelerator_tb;
 
     always @(posedge clk) begin
         if (data_ready) begin
-            $display("Result: %d", result);
+            $display("Result: %d", result); 
+            // Should be 11014
+            // 11014 / 4096 ~= 2.88
             $finish;
         end
     end
 
     initial begin
-        #100 $finish;
+        #300 $finish;
     end
 
 endmodule
